84cad1a0c38baf9f2cae93d96baaa8d6835b9f19
icaro-davi <icarodaviduarte@gmail.com>
2022-10-09 23:14:06 -0300

    feat: middleware check guilds

---
+++ src/middleware/verifyUserCanModifyGuild.middleware.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+import UserApplication from "../application/user.application";
+
+/**
+ * This middleware only works when pass a guild id as param
+ */
+async function verifyUserCanModifyGuild(req: Request, res: Response, next: NextFunction) {
+    const { id } = req.params;
+    const isAllowedToModifyGuild = await UserApplication.verifyGuildPermissions(id, { user: req.user! });
+    if(isAllowedToModifyGuild) {
+        next()
+    } else {
+        res.status(httpStatus.UNAUTHORIZED).send({ err: httpStatus[httpStatus.UNAUTHORIZED] })
+    }
+}
+
+export default { middleware: verifyUserCanModifyGuild, ignore: true };
