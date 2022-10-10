8efa820a0914854acf3bd68e294340d3a124e8f5
icaro-davi <icarodaviduarte@gmail.com>
2022-10-09 23:56:28 -0300

    change: set guilds in cache

--- src/controller/user/index.ts
+++ src/controller/user/index.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from "express";
 import UserApplication from "../../application/user.application";
+import AppCache from "../../lib/AppCache";
 
 export async function guilds(req: Request, res: Response) {
     const guilds = await UserApplication.getGuildsWithManagePermission(req.user!.accessToken);
+    AppCache.save(AppCache.createKey('GUILDS_USER_ID', req.user?._id), guilds);
     res.send(guilds);
 }
 
-export async function me(req: Request, res: Response){
-    console.log('Request')
+export async function me(req: Request, res: Response) {
     const user = await UserApplication.getMe(req.user!.accessToken);
-    console.log('user', user)
     res.send(user);
 }
