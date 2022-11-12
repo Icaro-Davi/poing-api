5c554dedd2b5e28400bb4c9c6b625a2a209d8bf3
Icaro Davi <icarodaviduarte@gmail.com>
2022-11-12 13:06:02 -0300

    change: get data from redis

--- src/controller/user/index.ts
+++ src/controller/user/index.ts
@@ -1,15 +1,12 @@
 import { Request, Response } from "express";
 import UserApplication from "../../application/user.application";
 import AppCache from "../../lib/AppCache";
-import { UserGuildsType } from "../../services/discord/user/types";
 
 export async function guilds(req: Request, res: Response) {
-    const guilds = await new Promise<UserGuildsType[]>((resolve, reject) => {
-        AppCache.saveAndGetData(
-            AppCache.createKey('GUILDS_USER_REF', req.user!.accessToken),
-            async () => UserApplication.getGuildsWithManagePermission(req.user!.accessToken)
-        ).then(resolve).catch(reject);
-    });
+    const guilds = await AppCache.saveAndGetData(
+        AppCache.createKey('GUILDS_USER_REF', req.user!.accessToken),
+        async () => UserApplication.getGuildsWithManagePermission(req.user!.accessToken)
+    );
     res.send(guilds);
 }
 
