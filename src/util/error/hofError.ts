73a0f82f03bd83bc9a574374a7cac084631d823d
Icaro Davi <icarodaviduarte@gmail.com>
2022-11-11 21:15:42 -0300

    fix: catch error inside promise

--- src/util/error/hofError.ts
+++ src/util/error/hofError.ts
@@ -3,8 +3,12 @@ import { NextFunction, Request, Response } from "express";
 type ControllerFunctionType = (req: Request<any>, res: Response, next?: NextFunction) => any | void;
 
 function useErrorHandler(fn: ControllerFunctionType) {
-    return function (req: Request, res: Response, next: NextFunction) {
-        Promise.resolve(fn(req, res, next)).catch(next);
+    return async function (req: Request, res: Response, next: NextFunction) {
+        try {
+            await fn(req, res, next);
+        } catch (error) {
+            next(error);
+        }
     }
 }
 
