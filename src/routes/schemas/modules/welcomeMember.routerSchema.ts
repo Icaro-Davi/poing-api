b48d92d9b379bd82fd6dd3c617d870b5cf26db28
Icaro Davi <icarodaviduarte@gmail.com>
2022-11-11 21:16:30 -0300

    change: validate sendChannelMessage body request

---
+++ src/routes/schemas/modules/welcomeMember.routerSchema.ts
@@ -0,0 +1,57 @@
+import Joi from "joi";
+import { createValidator } from "express-joi-validation";
+import { IWelcomeMemberModuleSettings, MessageEmbedType } from "../../../domain/db_poing/modules/welcomeModule/WelcomeModule.schema";
+
+const validator = createValidator();
+
+export const welcomeMember = {
+    settingsValidator: validator.body(
+        Joi.object<IWelcomeMemberModuleSettings>({
+            channelId: Joi.string().regex(/^\d+$/).max(50),
+            isMessageText: Joi.boolean().required(),
+            messageText: Joi.string().max(500),
+            messageEmbed: Joi.object<MessageEmbedType>({
+                author: Joi.object({
+                    name: Joi.string().max(50),
+                    picture: Joi.string().max(50)
+                }),
+                fields: Joi.array().items(
+                    Joi.object({
+                        name: Joi.string().required().max(100),
+                        value: Joi.string().required().max(150),
+                        inline: Joi.boolean()
+                    })
+                ),
+                title: Joi.string().max(100),
+                description: Joi.string().max(500),
+                footer: Joi.string().max(100),
+                thumbnail: Joi.string().max(50),
+            })
+        })
+    ),
+    settingsTestValidator: validator.body(
+        Joi.object<IWelcomeMemberModuleSettings>({
+            channelId: Joi.string().regex(/^\d+$/).max(50).required(),
+            isMessageText: Joi.boolean().required(),
+            messageText: Joi.string().max(500),
+            messageEmbed: Joi.object<MessageEmbedType & { color?: string }>({
+                author: Joi.object({
+                    name: Joi.string().max(100),
+                    picture: Joi.string().max(100)
+                }),
+                fields: Joi.array().items(
+                    Joi.object({
+                        name: Joi.string().required().max(100),
+                        value: Joi.string().required().max(150),
+                        inline: Joi.boolean()
+                    })
+                ),
+                color: Joi.string().max(7),
+                title: Joi.string().max(100),
+                description: Joi.string().max(500),
+                footer: Joi.string().max(100),
+                thumbnail: Joi.string().max(100),
+            })
+        })
+    )
+}
