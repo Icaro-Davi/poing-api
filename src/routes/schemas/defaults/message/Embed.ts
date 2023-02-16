66503c6345b79a18ccd99a7dfee2355b5584008f
Icaro Davi <icarodaviduarte@gmail.com>
2023-02-16 08:41:30 -0300

    fix: submit empty thumbnail

--- src/routes/schemas/defaults/message/Embed.ts
+++ src/routes/schemas/defaults/message/Embed.ts
@@ -16,7 +16,7 @@ const MessageEmbedSchema = (CustomConfigurations?: { [key: string]: any }) => Jo
     title: Joi.string().allow("").max(100),
     description: Joi.string().required().max(500),
     footer: Joi.string().allow("").max(100),
-    thumbnail: Joi.string().max(150),
+    thumbnail: Joi.string().allow("").max(150),
     ...CustomConfigurations
 });
 
