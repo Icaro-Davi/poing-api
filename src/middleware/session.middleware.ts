bea814b2fee30ede0ff4ec65e93c8c61ece299c5
icaro-davi <icarodaviduarte@gmail.com>
2024-05-21 23:06:12 -0300

    fix setting cookie from a wrong variable

--- src/middleware/session.middleware.ts
+++ src/middleware/session.middleware.ts
@@ -10,11 +10,13 @@ const ExpressSessionMiddleware = (app: Express) => {
         resave: false,
         saveUninitialized: false,
         cookie: {
-            sameSite: 'lax',
-            secure: true,
-            httpOnly: true,
             maxAge: 60000 * 60 * 24 * configs.env.session.cookieExpirationDays,
-            ...configs.env.session.cookieDomain ? { domain: configs.env.misc.WEB_APP_REDIRECT_URL } : {}
+            ...configs.env.session.cookieDomain ? {
+                sameSite: 'lax',
+                httpOnly: true,
+                secure: true,
+                domain: configs.env.session.cookieDomain
+            } : {}
         },
         store: MongoStore.create({
             mongoUrl: configs.env.db.POING_DASHBOARD_URI
