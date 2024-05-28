c944356c5018d2a7d8a5d1c1c615e5980a7bda07
icaro-davi <icarodaviduarte@gmail.com>
2024-05-27 23:40:28 -0300

    fix: using env value to apply cors domain on development

--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -3,10 +3,11 @@ import cors from 'cors';
 import configs from '../configs';
 
 const CorsMiddleware = (app: Express) => {
+    const allowedOrigin = configs.env.environment === 'development'
+        ? configs.env.server.allowedOrigins ?? new RegExp('^https?://localhost(:[\\d]{2,4})?$')
+        : configs.env.server.allowedOrigins;
     app.use(cors({
-        origin: configs.env.environment === 'development'
-            ? new RegExp('^https?://localhost(:[\\d]{2,4})?$')
-            : configs.env.server.allowedOrigins,
+        origin: allowedOrigin,
         credentials: true
     }));
 }
